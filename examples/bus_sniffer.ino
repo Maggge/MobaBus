42b6727c48eca930264b4e55f541a6d3e0d1f536
Markus Mair <magge.mair@gmail.com>
2021-12-17 10:51:09 +0100

    Add first examples

---
+++ examples/bus_sniffer.ino
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+
+#include <MobaBus.h>
+
+MobaBus mobaBus(); //MobaBus-controller instance
+
+MobaBus_CAN can(10, CAN_125KBPS, MCP_8MHZ, 2); //Can Bus interface
+
+void setup() {
+  Serial.begin(115200);
+ 
+  mobaBus.begin();
+  Serial.println("MobaBus");
+  Serial.println("--------------------------");
+
+  mobaBus.attachInterace(&can); //add the can bus interface to the controller
+
+}
+
+void loop() {
+
+  MobaBus_Packet pkg;
+  if(mobaBus.loop(&pkg)){
+    Serial.println("MobaBus - Packet received!");
+    Serial.print("Dev Type = 0x");
+    Serial.println(pkg.meta.type, HEX);
+    Serial.print("Address = 0x");
+    Serial.println(pkg.meta.address, HEX);
+    Serial.print("Cmd = 0x");
+    Serial.println(pkg.meta.cmd, HEX);
+    Serial.print("Data Len = ");
+    Serial.println(pkg.len);
+    Serial.print("DATA: ");
+    for (int i = 0; i < pkg.len; i++){
+      Serial.print(pkg.data[i]);
+      Serial.print("; ");
+    }
+    Serial.println();
+    Serial.println();
+  }
+}
---
+++ examples/servo16x.ino
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+#include <MobaBus.h>
+
+MobaBus mobaBus(0, 8, 9); //MobaBus-controller instance with EEPROM, progamming Button and statusLED
+
+MobaBus_CAN can(10, CAN_125KBPS, MCP_8MHZ, 2); //Can Bus interface
+
+MobaBus_TurnoutPCA servos(0x40, 70, 110, 230, true); // PCA9685 board with address=0x40, angle0=70°, angle1=110°, moving speed=230 and autoPowerOff=on
+
+void setup() {
+
+  mobaBus.begin(); //initialize a controller instance
+  mobaBus.attachInterace(&can); //add the can bus interface to the controller
+  mobaBus.attachModule(&servos); // add the PCA9685 module to the controller
+}
+
+void loop() {
+
+  mobaBus.loop();
+}
